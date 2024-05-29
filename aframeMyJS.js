a4d49700aa65f71a2bed789c84695d8c97c29b1e
jaz211081 <74247918+jaz211081@users.noreply.github.com>
2024-05-29 17:40:06 +0800

    renew

--- aframeMyJS.js
+++ aframeMyJS.js
@@ -1,23 +1,47 @@
 AFRAME.registerComponent('btngoogle', {
   init: function() {
-    this.el.addEventListener('click', (e) => {
+    this.el.addEventListener('click', function() {
       window.open('https://google.com');
-    })
+    });
   }
-})
-  
+});
+
 AFRAME.registerComponent('btnxc', {
-    init: function() {
-      this.el.addEventListener('click', (e) => {
-          window.open('https://xinchuang27442943.github.io/CompanyWebPage/');
-    })
+  init: function() {
+    this.el.addEventListener('click', function() {
+      window.open('https://xinchuang27442943.github.io/CompanyWebPage/');
+    });
   }
-})
+});
 
 AFRAME.registerComponent('btnhan', {
   init: function() {
-    this.el.addEventListener('click', (e) => {
+    this.el.addEventListener('click', function() {
       window.open('https://mrhan.mystrikingly.com/');
-    })
+    });
   }
-})
+});
+
+AFRAME.registerComponent('click-show-window', {
+  init: function () {
+    var el = this.el;
+    el.addEventListener('click', function () {
+      var fileName = el.getAttribute('data-file');
+      if (fileName) {
+        var infoWindow = document.getElementById('infoWindow');
+        fetch(fileName)
+          .then(response => response.text())
+          .then(data => {
+            document.getElementById('infoContent').innerHTML = data; // Use innerHTML to allow HTML tags
+            infoWindow.style.display = 'block';
+          })
+          .catch(error => console.error('Error loading message:', error));
+      }
+    });
+  }
+});
+
+function closeWindow() {
+  var infoWindow = document.getElementById('infoWindow');
+  infoWindow.style.display = 'none';
+}
